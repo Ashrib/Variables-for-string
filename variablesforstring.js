45c13d18aae5732f6446a86b1eb4d4a2024a6ac3
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-05-21 13:14:31 +0500

    fonts

---
+++ variablesforstring.js
@@ -0,0 +1,36 @@
+// variable for string
+// 1...            
+var userName="";
+// 2....
+var myName="Asharib Ali";
+// 3...
+var message="Hello World";
+alert(message);
+// 4...
+var stdName="Ali Khan";
+var stdAge="17 years";
+var stdInstitute="SSUET";
+var stdCourse="Mobile and computing - Module A";
+alert(stdName);
+alert(stdAge);
+alert(stdInstitute);
+alert(stdCourse);
+// 5...
+var pizza="PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(pizza);
+// 6...
+nbr_of_children=2;
+partner="ABC";
+lcation="USA";
+job="Software Developer";
+alert("You will be a" + " " + job + " " + "in" + " " + lcation + "," + "and married to" + " " + partner + " " + "with"+ " "+ nbr_of_children +" kids.");
+// 7...
+var email="abc@xyz.com";
+alert("My email address is"+ " "+ email);
+// 8...
+var book ="'A smarter way to learn JavaScript'";
+alert("I am trying to learn from the book"+ " "+ book);
+// 9...
+alert("My name is"+" "+ stdName + ", " + "I am"+" "+ stdAge +" old"+ ", " + "I study in"+ " "+ stdInstitute + ", "+ "I take"+" "+ stdCourse + " "+ "classes");
+//10...
+document.write("Yay! I can write the HTML content through Javascript");
